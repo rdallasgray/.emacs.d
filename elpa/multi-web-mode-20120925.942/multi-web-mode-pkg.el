9bea1976695edbec897d818f26d9ee3380c2aeb8
Robert Dallas Gray <mail@robertdallasgray.com>
2012-11-25 13:25:05 +0000

    Updated packages, added coffeescript yasnippets as submodule.

--- elpa/multi-web-mode-20120925.942/multi-web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "multi-web-mode" "20120925.942" "multiple major mode support for web editing [source: github]" 'nil)
-
--- elpa/s-20121108.2014/s-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "s" "20121108.2014" "The long lost Emacs string manipulation library. [source: github]" (quote nil))
