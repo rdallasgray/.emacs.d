efbb16dbd1d0e6cfceb0e5ce91425698d2375b64
Robert Dallas Gray <robert.dallas.gray@huzutech.com>
2012-12-11 09:15:49 +0000

    Pruned packages.

--- elpa/dash-20121124.1649/dash-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dash" "20121124.1649" "A modern list library for Emacs [source: github]" (quote nil))
--- elpa/php-mode-20121202.3/php-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "php-mode" "20121202.3" "major mode for editing PHP code [source: github]" (quote nil))
